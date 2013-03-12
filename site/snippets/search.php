64918428d85b5a9f2f5ffa49ad0c59840da4e7bf
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-12 02:17:49 +0000

    Add short titles

--- site/snippets/search.php
+++ site/snippets/search.php
@@ -1,6 +1,7 @@
-<form role="search" id="search" action="/search">
-    <div class="container">
-        <input type="search" class="input" name="q" placeholder="Search&#8230;"/><!--
-     --><input type="submit" class="button" value="Search"/>
-    </div>
-</form><!--/@search-->
+    <form role="search" id="search" action="/search">
+        <div class="container">
+            <input type="search" class="input" name="q" placeholder="Search&#8230;"/><!--
+         --><input type="submit" class="button" value="Search"/>
+        </div>
+    </form><!--/@search-->
+
