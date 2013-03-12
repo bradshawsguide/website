5ca28bdd500ce6eaf4584da996206c25c710b81c
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-12 02:18:29 +0000

    White space

--- site/snippets/banner.php
+++ site/snippets/banner.php
@@ -1,7 +1,7 @@
-<header id="top" role="banner">
-    <div class="container">
-        <h1><a href="<?= url() ?>"><?= smartypants($site->shorttitle()) ?></a></h1>
-        <a href="#nav">Jump to navigation</a>
-        <a href="#search">Jump to search</a>
-    </div>
-</header><!--/@banner-->
+    <header id="top" role="banner">
+        <div class="container">
+            <h1><a href="<?= url() ?>"><?= smartypants($site->shorttitle()) ?></a></h1>
+            <a href="#nav">Jump to navigation</a>
+            <a href="#search">Jump to search</a>
+        </div>
+    </header><!--/@banner-->
