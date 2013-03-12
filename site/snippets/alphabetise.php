7ae140b2c03bd705b963b3281c475f7875e321e8
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-12 02:18:21 +0000

    White space and entity encoding in alphabetised listings

--- site/snippets/alphabetise.php
+++ site/snippets/alphabetise.php
@@ -7,10 +7,16 @@
     foreach($alphabetise as $letter => $items):
 ?>
     
-        <h2 class="index"><?php echo str::upper($letter) ?></h2>
-        <ul class="<?= $type ?> listing">
-        <?php foreach($items as $item): ?>
-    <li><a href="<?= $item->url() ?>"<? if ($item->text() == ''): ?> class="unremarkable"<? endif ?>><?= smartypants($item->title()) ?></a></li>
-        <?php endforeach ?>
-</ul>
+            <h2 class="index"><?php echo str::upper($letter) ?></h2>
+            <ul class="<?= $type ?> listing">
+<?php foreach($items as $item):
+    if ($item->shorttitle() != null) {
+        $title = $item->shorttitle();
+    } else {
+        $title = $item->title();
+    }
+?>
+                <li><a href="<?= $item->url() ?>"<? if ($item->text() == ''): ?> class="unremarkable"<? endif ?>><?= smartypants(html($title)) ?></a></li>
+<?php endforeach ?>
+            </ul>
 <?php endforeach ?>
