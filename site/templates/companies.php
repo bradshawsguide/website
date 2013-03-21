78b504308e2dddf5d8652e6137f028a6980aad84
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-21 02:05:45 +0000

    Custom listing pages for Companies
    Companies page ~= section content page in book version. Trying to bring
    the two closer together.

---
+++ site/templates/companies.php
@@ -0,0 +1,30 @@
+<? snippet('header') ?>
+<? snippet('banner') ?>
+<? $type = "stations"?>
+
+    <main role="main">
+        <section class="container">
+            <header>
+                <h1><?= smartypants($page->title()) ?></h1>
+            </header>
+
+<?          $items = $pages->findOpen()->children->sortBy('title', 'asc'); ?>
+<?          if($items && $items->count()): ?>
+<?              foreach($items as $item): ?>
+                <h2><a href="<?= $item->url() ?>"><?= smartypants($item->title()) ?></a></h2>
+<?              $company = $item->title();
+                $lines = $pages->find('lines')->children()->filterBy('company', "$company")->sortBy('title', 'asc'); ?>
+                <ul class="listing">
+<?                  foreach($lines as $line): ?>
+                    <li><a href="<?= $line->url() ?>"><?= smartypants($line->title().' '.$line->type()) ?></a></li>
+<?                  endforeach ?>
+                </ul>
+<?              endforeach ?>
+<?          endif ?>
+        </section>
+    </main><!--/@main-->
+
+<? snippet('navigation') ?>
+<? snippet('search') ?>
+<? snippet('contentinfo') ?>
+<? snippet('footer') ?>
