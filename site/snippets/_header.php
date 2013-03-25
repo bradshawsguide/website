969023575293c9a2ca78a912fc3b4cacec5cfb25
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-25 23:11:49 +0000

    Simplify header and footer (require less recursive includes)

--- site/snippets/_header.php
+++ site/snippets/_header.php
@@ -1,6 +1,31 @@
-<?
-    if (!isset($_GET['ajax'])) {
-        snippet('header');
-        snippet('banner');
-    }
-?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+    <title><?php if ($page->isHomePage() == false) : ?><?= smartypants($page->title()) ?> - <?php endif ?><?= smartypants($site->title()) ?></title>
+    <meta charset="utf-8" />
+    <meta name="description" content="<?= html($site->description()) ?>"/>
+    <meta name="keywords" content="<?= html($site->keywords()) ?>"/>
+    <meta name="robots" content="index, follow"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="application-name" content="<?= smartypants($site->shorttitle()) ?>">
+    <meta name="apple-mobile-web-app-title" content="<?= smartypants($site->shorttitle()) ?>">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <link rel="apple-touch-icon-precomposed" href="<?= url('assets/images/apple-touch-icon.png') ?>"/>
+    <link rel="apple-touch-startup-image" href="<?= url('assets/images/apple-touch-startup-image.png') ?>" media="(device-width: 320px)"/>
+    <link rel="apple-touch-startup-image" href="<?= url('assets/images/apple-touch-startup-image@2x.png') ?>" media="(device-width: 320px) and (-webkit-device-pixel-ratio: 2)">
+    <link rel="icon" href="<?= url('assets/images/favicon.png') ?>" type="image/png"/>
+    <? $lesscss = lesscss('/assets/styles/less/styles.less','/assets/styles/styles.css') ?>
+<?= css('assets/styles/styles.css') ?>
+</head>
+
+<body>
+    <header role="banner" id="top">
+        <div class="container">
+            <h1><a href="<?= url() ?>"><?= smartypants($site->shorttitle()) ?></a></h1>
+            <a href="#nav">Jump to navigation</a>
+            <a href="#search">Jump to search</a>
+        </div>
+    </header><!--/@banner-->
+
+    <main role="main" id="main">
--- site/snippets/contentinfo.php
+++
@@ -1,8 +0,0 @@
-    <footer role="contentinfo">
-        <div class="container">
-            <p><small>&#169; <abbr title="2013">MMXIII</abbr> <a href="http://paulrobertlloyd.com/">Paul Robert Lloyd</a></small></p>
-            <!--p><small xmlns:dct="http://purl.org/dc/terms/">This work (<cite property="dct:title"><?= smartypants($site->title()) ?></cite>, by <a href="http://bradshaws.co/" rel="dct:creator"><span property="dct:title"><?= smartypants($site->author()) ?></span></a>), identified by <a href="<?= $site->publisherurl() ?>" rel="dct:publisher"><span property="dct:title"><?= smartypants($site->publisher()) ?></span></a>, is <a rel="license" href="<?= $site->licenseurl() ?>">free of known copyright restrictions</a>.</small></p-->
-            <p>Made with <a href="http://getkirby.com/">Kirby</a>. Hosted by <a href="#mediatemple.net">(mt)mediatemple</a>.</p>
-            <p><a rel="me" href="http://twitter.com/bradshawsguide">Follow George Bradshaw on Twitter</a></p>
-        </div>
-    </footer><!--/@contentinfo-->
