8502ada797e95698fe547eb09d7bb34c32875af2
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-26 01:25:21 +0000

    Temporarily link to search page directly

--- site/snippets/_header.php
+++ site/snippets/_header.php
@@ -24,7 +24,7 @@
         <div class="container">
             <h1><a href="<?= url() ?>"><?= smartypants($site->shorttitle()) ?></a></h1>
             <a href="#nav">Jump to navigation</a>
-            <a href="#search">Jump to search</a>
+            <a href="/search">Jump to search</a>
         </div>
     </header><!--/@banner-->
 
