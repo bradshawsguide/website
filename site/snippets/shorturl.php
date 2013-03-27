c41a9d5d725a4336adad92de5a493e08d7daabb8
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-03-27 00:36:13 +0000

    Hard-code short URL domain (bradshaws.co)

--- site/snippets/shorturl.php
+++ site/snippets/shorturl.php
@@ -1,5 +1,5 @@
             <section class="shorturl">
                 <h1>Short URL</h1>
-                <a href="<?= $page->tinyurl() ?>"><?= str::shorturl($page->tinyurl()) ?></a>
+                <a href="http://bradshaws.co<?= $page->tinyurl() ?>"><?= "bradshaws.co".$page->tinyurl() ?></a>
             </section><!--/.shorturl-->
 
