81ab38ee15bca0bccce6264fdd626d2fbfa4c243
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-04-03 00:52:50 +0100

    Let regions have page navigation

--- site/templates/region.php
+++ site/templates/region.php
@@ -1,5 +1,4 @@
 <? if (!isset($_GET['ajax'])) { snippet('_header'); } ?>
-
         <article>
             <header>
                 <hgroup>
@@ -12,6 +11,32 @@
 <?=             kirbytext($page->text()); ?>
             </div>
 
+<?      if ($page->type() == 'parent'):
+            $items = $page->children()->visible();
+            if($items && $items->count()): ?>
+            <nav role="navigation">
+                <h1><a href="<?= $page->url(); ?>"><?= $page->title(); ?></a></h1>
+                <ul>
+<?                  foreach($items as $item): ?>
+                    <li><a<?= ($item->isOpen()) ? ' class="is-active"' : '' ?> href="<?= $item->url() ?>"><?= html($item->title()) ?></a></li>
+<?                  endforeach ?>
+                </ul>
+            </nav>
+<?          endif ?>
+<?      elseif ($page->type() == 'child'):
+            $items = $page->siblings()->visible();
+            if($items && $items->count()): ?>
+            <nav role="navigation">
+                <h1><a href="<?= $page->parent()->url(); ?>"><?= $page->parent()->title(); ?></a></h1>
+                <ul>
+<?                  foreach($items as $item): ?>
+                    <li><a<?= ($item->isOpen()) ? ' class="is-active"' : '' ?> href="<?= $item->url() ?>"><?= html($item->title()) ?></a></li>
+<?                  endforeach ?>
+                </ul>
+            </nav>
+<?          endif ?>
+<?      endif ?>
+
 <?          if($page->related()): ?>
             <section>
                 <h1>Related</h1>
@@ -23,23 +48,24 @@
             </section>
 <?          endif ?>
 
+<?          $region = $page->title();
+            $search = $pages->find('stations')->children->filterBy('region', "$region")->sortBy('title', 'asc');
+            
+            if ($search != ""): ?>
             <section class="stations index">
                 <h1>Stations in This County</h1>
-<?php
-                $region = $page->title();
-                $alphabetise = alphabetise($pages->find('stations')->children->filterBy('region', "$region")->sortBy('title', 'asc'));
-    
-                foreach($alphabetise as $letter => $items):
-?>
-                <h2 class="index"><?php echo str::upper($letter) ?></h2>
+<?              $alphabetise = alphabetise($search);
+                foreach($alphabetise as $letter => $items): ?>
+                <h2 class="index"><?= str::upper($letter) ?></h2>
                 <ul class="stations listing">
-<?php               foreach($items as $item): ?>
+<?                  foreach($items as $item): ?>
                     <li><a href="<?= $item->url() ?>"<? if ($item->text() == ''): ?> class="unremarkable"<? endif ?>><?= smartypants($item->title()) ?></a></li>
-<?php               endforeach ?>
+<?                  endforeach ?>
                 </ul>
-<?php           endforeach ?>
+<?              endforeach ?>
+<?          endif ?>
             </section>
+<?          snippet('shorturl') ?>
 <?          snippet('prevnext') ?>
         </article>
-
 <? if (!isset($_GET['ajax'])) { snippet('_footer'); } ?>
