566996bd5d83dcc89559559eceaa5a16ad6ed814
Paul Lloyd <github@paulrobertlloyd.com>
2013-04-07 02:41:05 +0100

    Include link to Foursquare in footer, and update small print.

--- site/snippets/_footer.php
+++ site/snippets/_footer.php
@@ -19,10 +19,12 @@
     </form><!--/@search-->
 
     <footer role="contentinfo">
-        <p><small>&#169; <abbr title="2013">MMXIII</abbr> <a href="http://paulrobertlloyd.com/">Paul Robert Lloyd</a></small></p>
-        <!--p><small xmlns:dct="http://purl.org/dc/terms/">This work (<cite property="dct:title"><?= smartypants($site->title) ?></cite>, by <a href="http://bradshaws.co/" rel="dct:creator"><span property="dct:title"><?= smartypants($site->author) ?></span></a>), identified by <a href="<?= $site->publisherurl ?>" rel="dct:publisher"><span property="dct:title"><?= smartypants($site->publisher) ?></span></a>, is <a rel="license" href="<?= $site->licenseurl ?>">free of known copyright restrictions</a>.</small></p-->
-        <p>&#160;</p>
-        <p><a rel="me" href="http://twitter.com/bradshawsguide">Follow George Bradshaw on Twitter</a></p>
+        <p><small>&#169; <abbr title="2013">MMXIII</abbr> <a href="http://paulrobertlloyd.com">Paul Robert Lloyd</a></small></p>
+        <p>Made with <a href="http://getkirby.com">Kirby</a>. Hosted by <a href="http://webfaction.com">WebFaction</a>. <a href="/about/colophon">Colphon</a></p>
+        <ul>
+            <li><a rel="me" href="http://foursquare.com/bradshawsguide">Follow George Bradshaw on Foursquare</a></li>
+            <li><a rel="me" href="http://twitter.com/bradshawsguide">Follow George Bradshaw on Twitter</a></li>
+        </ul>
     </footer><!--/@contentinfo-->
 
     <?= js('assets/scripts/scripts.js') ?>
