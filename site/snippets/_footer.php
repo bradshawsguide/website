00d861a13f8a3eb98bfd27d7912df78fcf595f6f
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-04-08 00:48:40 +0100

    Typo

--- site/snippets/_footer.php
+++ site/snippets/_footer.php
@@ -20,7 +20,7 @@
 
     <footer role="contentinfo">
         <p><small>&#169; <abbr title="2013">MMXIII</abbr> <a href="http://paulrobertlloyd.com">Paul Robert Lloyd</a></small></p>
-        <p>Made with <a href="http://getkirby.com">Kirby</a>. Hosted by <a href="http://webfaction.com">WebFaction</a>. <a href="/about/stylegide">Styleguide</a></p>
+        <p>Made with <a href="http://getkirby.com">Kirby</a>. Hosted by <a href="http://webfaction.com">WebFaction</a>. <a href="/about/styleguide">Styleguide</a></p>
         <ul>
             <li><a rel="me" href="https://foursquare.com/bradshawsguide">Follow George Bradshaw on Foursquare</a></li>
             <li><a rel="me" href="https://twitter.com/bradshawsguide">Follow George Bradshaw on Twitter</a></li>
