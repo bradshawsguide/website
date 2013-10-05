6909a82af0b1df595d07f51004e92e197793eeb7
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2013-10-05 17:59:25 +0100

    Include charset to please W3C validator

--- site/snippets/_header.php
+++ site/snippets/_header.php
@@ -59,6 +59,7 @@
     <link rel="author" href="<?= url('humans.txt') ?>"/>
  <!--link rel="webmention" href="<?= url('webmention/') ?>"/-->
 
+    <meta charset="utf-8"/>
     <meta name="robots" content="index, follow"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="application-name" content="<?= smartypants($site->shorttitle) ?>">
