b0fbf2b6849db845364d8cfe860c8251c5f9b523
Paul Lloyd <paulrobertlloyd@Pinova.local>
2013-12-11 21:14:16 +0000

    Link to correct URL

--- site/config/config.bradshawsguide.org.php
+++ site/config/config.bradshawsguide.org.php
@@ -1,5 +1,5 @@
 <?php
 c::set('cache', true);
 //c::set('staticurl', '//static.bradshawsguide.org/');
-c::set('staticurl', '//bradshawsguide.org.global.prod.fastly.net/');
+c::set('staticurl', '//static.bradshawsguide.org.global.prod.fastly.net/');
 ?>
