bec3db9f34aabc8561f284227bb20218f407be1d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2015-12-26 23:45:09 +0000

    Update dates, publisher URL and other meta content

--- site/snippets/_footer.php
+++ site/snippets/_footer.php
@@ -23,7 +23,7 @@
     </form><!--/@search-->
 
     <footer role="contentinfo">
-        <p><small>&#169; <abbr title="2013">MMXIII</abbr> <a href="http://paulrobertlloyd.com">Paul Robert Lloyd</a></small></p>
+        <p><small>&#169; <abbr title="2016">MMXVI</abbr> <a href="https://paulrobertlloyd.com">Paul Robert Lloyd</a></small></p>
         <p><a href="/about/colophon">Colophon</a>. <a href="/about/styleguide">Styleguide</a></p>
         <ul>
             <li><a rel="me" href="https://foursquare.com/bradshawsguide">Follow George Bradshaw on Foursquare</a></li>
