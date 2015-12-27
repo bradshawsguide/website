c91099ae8f48fa44384749b67ab23dd7bf61c815
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2015-12-27 16:29:15 +0000

    Fix prev/next titles

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -18,9 +18,9 @@ if ($page->hasNext() == true) {
 <nav class="prevnext">
     <h1 class="hidden">Previous and Next <?= $page->parent()->title() ?></h1>
     <? if ($page->hasPrev() == true): ?>
-        <a href="<?= $page->prev()->url() ?>" rel="prev"><span><?= smartypants($prevTitle) ?></span></a>
+        <a href="<?= $page->prev()->url() ?>" rel="prev"><span><?= smartypants($page->prev()->title()) ?></span></a>
     <? endif ?>
     <? if ($page->hasNext() == true): ?>
-        <a href="<?= $page->next()->url() ?>" rel="next"><span><?= smartypants($nextTitle) ?></span></a>
+        <a href="<?= $page->next()->url() ?>" rel="next"><span><?= smartypants($page->next()->title()) ?></span></a>
     <? endif ?>
 </nav><!--/.prevnext-->
