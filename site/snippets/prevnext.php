b452826db9a0dc46a597e748e8919dd54095bf20
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2015-12-29 17:34:56 +0000

    Fix navigation

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -1,26 +1,11 @@
-<?php
-if ($page->hasPrev() == true) {
-    if ($page->prev()->short_title()->isNotEmpty()) {
-        $prevTitle = $page->prev()->short_title();
-    } else {
-        $prevTitle = $page->prev()->title();
-    }
-}
-
-if ($page->hasNext() == true) {
-    if ($page->next()->short_title()->isNotEmpty()) {
-        $nextTitle = $page->next()->short_title();
-    } else {
-        $nextTitle = $page->next()->title();
-    }
-}
-?>
 <nav class="prevnext">
     <h1 class="hidden">Previous and Next <?= $page->parent()->title() ?></h1>
-    <? if ($page->hasPrev() == true): ?>
-        <a href="<?= $page->prev()->url() ?>" rel="prev"><span><?= smartypants($nextTitle) ?></span></a>
-    <? endif ?>
-    <? if ($page->hasNext() == true): ?>
-        <a href="<?= $page->next()->url() ?>" rel="next"><span><?= smartypants($prevTitle) ?></span></a>
-    <? endif ?>
+
+<? if ($page->hasPrev() == true): ?>
+    <a href="<?= $page->prev()->url() ?>" rel="prev"><span><?= smartypants($page->prev()->title()) ?></span></a>
+<? endif ?>
+
+<? if ($page->hasNext() == true): ?>
+    <a href="<?= $page->next()->url() ?>" rel="next"><span><?= smartypants($page->next()->title()) ?></span></a>
+<? endif ?>
 </nav><!--/.prevnext-->
