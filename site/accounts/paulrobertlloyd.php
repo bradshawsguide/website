b37fd5f63858f6fdd492edea43b8b55f9235e628
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-01-19 21:46:06 +0000

    YAML for panel blueprints

--- site/accounts/paulrobertlloyd.php
+++ site/accounts/paulrobertlloyd.php
@@ -3,7 +3,7 @@
 username: paulrobertlloyd
 email: me+bradshawsguide@paulrobertlloyd.com
 password: >
-  $2a$10$mXFSpSAPTFvE78OkOSAhlez1h2RbJ3O3y6STbxt8eW3sUgIp/vY.2
+  $2a$10$pc9z27tQkOHx2KtsdUvkh.Wcjhzlu9uFT/MvfLPjh99pcXEXAFzDu
 language: en
 role: admin
 firstname: Paul
