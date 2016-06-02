c081317f0e90ad60dc5b0e092a1a198bbba5b2df
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-06-02 19:55:10 +0100

    Fix routemap pattern

--- src/patterns/section/routemap/routemap.html.php
+++ src/patterns/section/routemap/routemap.html.php
@@ -1,39 +1,40 @@
 <? if($stops): ?>
-  <section class="c-section c-section--route">
-    <h1 class="c-section__title">Route Map</h1>
-    <ol class="c-routemap">
-    <? foreach($stops as $stop):
-      // Get page array for item in `stops:` YAML list
-      $stop = page('stations/'.$stop);
+<section class="c-section c-section--route">
+  <h1 class="c-section__title">Route Map</h1>
+  <ol class="c-routemap">
+  <? foreach($stops as $stop):
+    // Get page array for item in `stops:` YAML list
+    $stop = page('stations/'.$stop);
 
-      // Get `routes` YAML list in this stop
-      $routes = $stop->route()->yaml();
+    // Get `routes` YAML list in this stop
+    $routes = $stop->route()->yaml();
 
-      foreach ($routes as $route) {
-        // Get page array for item in `route:` YAML list
-        $route = page('routes/'.$route);
+    foreach ($routes as $route) {
+      // Get page array for item in `route:` YAML list
+      $route = page('routes/'.$route);
 
-        if ($route !== $page) {
-          // Build link to terminating station on each branch
-          foreach ($routes as $branch) {
+      if ($route !== $page) {
+        $type = 'interchange';
+      } else {
+        $type = 'station';
+      }
+    }
+  ?>
+    <li class="c-routemap__<?= $type ?>">
+      <?= html::a($stop->url(), smartypants($stop->title())) ?>
+      <? if ($type == "interchange"): ?>
+      <ul>
+        <?
+          foreach ($routes as $branch):
             $branch = page('routes/'.$branch);
             $terminus = a::last(str::split($branch->title(), $separator=' to ', $length=1));
-          }
-          $type = 'interchange';
-        } else {
-          $type = 'station';
-        }
-      }
-    ?>
-      <li class="c-routemap__<?= $type ?>">
-        <a href="<?= $stop->url() ?>"><?= smartypants($stop->title()) ?></a>
-        <? if ($type == "interchange"): ?>
-        <ul>
-          <li><?= html::a($branch->url(), smartypants($terminus)) ?></li>
-        </ul>
-        <? endif ?>
-      </li>
-    <? endforeach ?>
-    </ol>
-  </section>
+        ?>
+        <li><?= html::a($branch->url(), smartypants($terminus)) ?></li>
+        <? endforeach ?>
+      </ul>
+      <? endif ?>
+    </li>
+  <? endforeach ?>
+  </ol>
+</section>
 <? endif ?>
