2db8eb30d09415aea0b7d398260367c8ed4015aa
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-11-28 02:09:05 +0000

    Search result description styling

--- src/patterns/common/results/results.html.php
+++ src/patterns/common/results/results.html.php
@@ -7,8 +7,8 @@
       <em class="c-result__type"><?= str::ucfirst($result->content()->name()) ?></em>
       <span class="u-hidden">)</span>
     </h2>
-    <div class="c-result__desc s-prose">
-      <p><?= excerpt($result->text(), $length=140); ?></p>
+    <div class="c-result__desc">
+      <p><?= excerpt($result->text(), $length=240); ?></p>
     </div>
     <p class="c-result__url"><?= server::get('server_name'); ?><?= $result->url() ?></p>
   </a>
