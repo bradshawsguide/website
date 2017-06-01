9c35ee728d5ad9c7e556cfb48c81a39d031d4fdf
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-06-02 00:58:04 +0100

    White space in custom plugins

--- src/plugins/robots/robots.php
+++ src/plugins/robots/robots.php
@@ -1,17 +1,14 @@
 <?php
 
 kirby()->routes(array(
-	array(
-		'pattern' => 'robots.txt',
-		'action'	=> function() {
-			return new Response('User-agent: *
-Disallow: /content/*.txt$
+  array(
+    'pattern' => 'robots.txt',
+    'action'	=> function() {
+      return new Response('User-agent: *
 Disallow: /kirby/
-Disallow: /site/
 Disallow: /panel/
-Disallow: /*.md$
 
-Sitemap: ' . u('sitemap.xml'), 'txt');
-		}
-	)
+Sitemap: '.u('sitemap.xml'), 'txt');
+    }
+  )
 ));
