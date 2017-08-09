a3700d918e4a47d7e009f7dc545160a596d2f5d6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-08-09 20:25:55 +0100

    Search styles

--- src/patterns/global/search/search.html.php
+++ src/patterns/global/search/search.html.php
@@ -1,7 +1,7 @@
 <div class="c-search" role="search">
-  <form action="/search">
-    <label class="c-search__label">Search <?= $site->title() ?></label>
-    <input class="c-search__input" type="search" name="q" placeholder="e.g. Brighton, Windsor Castle, Surrey…"<? if(isset($search)): ?> value="<?= esc($query) ?><? endif ?>"/>
+  <form class="c-search__form" action="/search">
+    <label class="c-search__label" for="query">Search <?= $site->title() ?></label>
+    <input class="c-search__input" type="search" id="query" name="q" placeholder="e.g. Brighton, Windsor Castle, Surrey…"<? if(isset($search)): ?> value="<?= esc($query) ?><? endif ?>"/>
     <button class="c-search__button" type="submit">
       <? pattern('common/icon', [
         'glyph' => 'search',
