feabf630211bdc3c181f3eb47765bb8a9e337594
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-08-13 01:05:36 +0100

    Actually fix header pattern

--- src/patterns/common/page/header/header.html.php
+++ src/patterns/common/page/header/header.html.php
@@ -5,11 +5,11 @@
 ?>
 <header class="c-page__header<?= $mods ?? '' ?>">
   <div class="c-page__header-inset">
-  <? if(isset($parent)): ?>
+  <? if(isset($parent) && !$parent->empty()): ?>
     <nav class="c-page__parent"><?= html::a($parent->url(), smartypants($parent->title())) ?></nav>
   <? endif ?>
     <h2 class="c-page__title"><?= smartypants($title) ?></h2>
-  <? if(!$subtitle->empty()): ?>
+  <? if(isset($subtitle) && !$subtitle->empty()): ?>
     <p class="c-page__subtitle"><?= smartypants($subtitle) ?></p>
   <? endif ?>
   </div>
