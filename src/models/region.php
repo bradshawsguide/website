ddb56646170ee6037b605bdc8b92b597536a501c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-08-22 23:01:25 +0100

    Fix region links

--- src/models/region.php
+++ src/models/region.php
@@ -9,4 +9,15 @@ class RegionPage extends Page {
 
     return $featured;
   }
+
+  // Return `title_short` if exists, else normal title
+  public function shortTitle() {
+    if (!$this->title_short()->empty()) {
+      $shortTitle = $this->title_short();
+    } else {
+      $shortTitle = $this->title();
+    };
+
+    return $shortTitle;
+  }
 };
