cd70a01c4e10f095090add0f92cae62db5205d4a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-10-13 12:21:07 +0100

    Highlight current route

--- src/patterns/common/route-traversal/route-traversal.html.php
+++ src/patterns/common/route-traversal/route-traversal.html.php
@@ -1,4 +1,7 @@
 <?
+  // Test if $route == ?route query
+  $currentRoute = kirby()->request()->query()->route();
+
   // Flatten array so that branch stops appear on main route
   $stops = flatten_array($route->stops()->yaml());
   $stopKey = array_search($page->uid(), $stops);
@@ -13,18 +16,19 @@
     $next = page('stations/'.$nextUID);
   }
 ?>
-<nav class="c-route-traversal">
+
+<nav class="c-route-traversal<? if ($currentRoute == $route->uid()): ?> c-route-traversal--current<? endif ?>">
   <?= brick('h'.(isset($level) ? $level : 3))->html($title)->attr('class', 'c-route-traversal__title') ?>
 
   <? if(isset($prev)): ?>
-    <a class="c-route-traversal__prev" rel="prev" href="<?= $prev->url() ?>" aria-label="Previous station: <?= $prev->shortTitle()?>">
+    <a class="c-route-traversal__prev" rel="prev" href="<?= $prev->url() ?>?route=<?= $route->uid() ?>" aria-label="Previous station: <?= $prev->shortTitle()?>">
       <?= smartypants($prev->shortTitle()) ?>
     </a>
   <? else: ?>
     <em class="c-route-traversal__prev">Terminus</em>
   <? endif ?>
   <? if(isset($next)): ?>
-    <a class="c-route-traversal__next" rel="next" href="<?= $next->url() ?>" aria-label="Next station: <?= $next->shortTitle()?>">
+    <a class="c-route-traversal__next" rel="next" href="<?= $next->url() ?>?route=<?= $route->uid() ?>" aria-label="Next station: <?= $next->shortTitle()?>">
       <?= smartypants($next->shortTitle()) ?>
     </a>
   <? else: ?>
