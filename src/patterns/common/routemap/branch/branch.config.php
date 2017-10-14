875a0f6eb3ef4cf64a7bdaf7b640822e36b2e37f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-10-14 01:26:34 +0100

    Remove routemap pattern

--- src/patterns/common/routemap/branch/branch.config.php
+++
@@ -1,6 +0,0 @@
-<?
-return [
-  'defaults' => [
-    'stops' => page('routes/london-bridge-to-brighton')->stops()->yaml()
-  ]
-];
--- src/patterns/common/routemap/station/station.config.php
+++
@@ -1,7 +0,0 @@
-<?
-
-return [
-  'defaults' => [
-    'station' => page('stations/brighton')
-  ]
-];
