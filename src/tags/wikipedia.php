a9a3fb4bd7b3f1e1f7c13ceadfc344adb9b91247
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-11-21 11:21:53 +0000

    Wikipedia kirbytext tag can accept text attribute

--- src/tags/wikipedia.php
+++ src/tags/wikipedia.php
@@ -1,10 +1,14 @@
 <?php
 
 kirbytext::$tags['wikipedia'] = array(
+    'attr' => array(
+        'text'
+    ),
     'html' => function ($tag) {
+        $text = $tag->attr('text', $tag->page()->currentTitle());
         $a = brick('a');
         $a->attr('href', 'https://en.wikipedia.org/wiki/'.$tag->attr('wikipedia'));
-        $a->html($tag->page()->currentTitle().' on Wikipedia');
+        $a->html($text.' on Wikipedia');
         return $a;
     }
 );
