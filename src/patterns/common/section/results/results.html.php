767fbf5d79b3513b1c23c1c381120408d3e6046b
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-12-25 09:48:24 +0000

    More fixes to search results

--- src/patterns/common/section/results/results.html.php
+++ src/patterns/common/section/results/results.html.php
@@ -2,8 +2,12 @@
 <?= brick('h'.(isset($level) ? $level : 2))->html($title)->attr('class', 'c-section__title') ?>
 <?php
     foreach ($results as $result) {
+        if ($result->intendedTemplate() == 'station') {
+            $result = $result->placePage();
+        }
+
         pattern('common/result', [
-            'result' => $result->placePage()
+            'result' => $result
         ]);
     }
 ?>
