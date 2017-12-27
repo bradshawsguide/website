b7a42a31f2670ff1b4db766f6fdae5cbee51cc5c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-12-27 11:53:52 +0000

    Update pagination component

--- src/patterns/common/pagination/pagination.html.php
+++ src/patterns/common/pagination/pagination.html.php
@@ -1,17 +1,25 @@
-<nav class="c-pagination">
-    <?php if ($pagination->hasPrevPage()): ?>
-        <a class="c-pagination__prev" rel="prev" href="<?= $pagination->prevPageURL() ?>" aria-label="Previous results page">Previous</a>
-    <?php else: ?>
-        <span class="c-pagination__prev" aria-hidden="true">Previous</span>
-    <?php endif ?>
+<nav class="c-pagination" aria-label="Pagination">
+    <ul class="c-pagination__items">
+        <li class="c-pagination__item">
+<?php if ($pagination->hasPrevPage()): ?>
+            <a href="<?= $pagination->prevPageURL() ?>" rel="prev" aria-label="Previous results page">Previous</a>
+<?php else: ?>
+            <span aria-hidden="true">Previous</span>
+<?php endif ?>
+        </li>
 
-    <?php foreach ($pagination->range(10) as $paging): ?>
-        <a href="<?= $pagination->pageURL($paging); ?>"><?= $paging; ?></a>
-    <?php endforeach ?>
+<?php foreach ($pagination->range(10) as $paging): ?>
+        <li class="c-pagination__item">
+            <a href="<?= $pagination->pageURL($paging); ?>" aria-label="Go to page <?= $paging; ?>"<?php e($paging == $pagination->page(), ' aria-current="page"') ?>><?= $paging; ?></a>
+        </li>
+<?php endforeach ?>
 
-    <?php if ($pagination->hasNextPage()): ?>
-        <a class="c-pagination__next" rel="next" href="<?= $pagination->nextPageURL() ?>" aria-label="Next results page">Next</a>
-    <?php else: ?>
-        <span class="c-pagination__prev" aria-hidden="true">Next</span>
-    <?php endif ?>
+        <li class="c-pagination__item">
+<?php if ($pagination->hasNextPage()): ?>
+            <a href="<?= $pagination->nextPageURL() ?>" rel="next" aria-label="Next results page">Next</a>
+<?php else: ?>
+            <span aria-hidden="true">Next</span>
+<?php endif ?>
+        </li>
+    </ul>
 </nav>
