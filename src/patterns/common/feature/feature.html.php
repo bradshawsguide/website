444a13e86bd7e161a6c7ccb3610b149c0cbedee6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-12-28 17:28:03 +0000

    Remove block-level links

--- src/patterns/common/feature/feature.html.php
+++ src/patterns/common/feature/feature.html.php
@@ -1,24 +1,25 @@
 <article class="c-feature">
-    <a class="c-feature__body" href="<?= $item->url() ?>">
-        <header class="c-feature__header">
-            <p class="c-feature__parent"><?= $item->parent()->title() ?></p>
-            <?= brick('h'.$level)->html($item->shortTitle())->addClass('c-feature__title') ?>
-        </header>
+    <header class="c-feature__header">
+        <?php
+            $title = html::a($item->url(), $item->title());
+            echo brick('h'.$level)->html($title)->addClass('c-feature__title')
+        ?>
+        <p class="c-feature__parent"><?= $item->parent()->title() ?></p>
+    </header>
 
-        <?php if ($item->hasImages()): ?>
-        <div class="c-feature__img u-artwork" style="background-image: url(<?= $item->image()->crop(16, 9)->dataUri() ?>)">
-            <img src="<?= $item->image()->crop(480, 270, 80)->url() ?>" alt="" width="224" height="128"/>
-        </div>
-        <?php endif; ?>
+    <?php if ($item->hasImages()): ?>
+    <div class="c-feature__img u-artwork" style="background-image: url(<?= $item->image()->crop(16, 9)->dataUri() ?>)">
+        <img src="<?= $item->image()->crop(480, 270, 80)->url() ?>" alt="" width="224" height="128"/>
+    </div>
+    <?php endif; ?>
 
-        <?php if ($item->excerpt()): ?>
-        <div class="c-feature__main">
-            <?php
-                pattern('scopes/text', [
-                    'content' => $item->excerpt()
-                ]);
-            ?>
-        </div>
-        <?php endif; ?>
-    </a>
+    <?php if ($item->excerpt()): ?>
+    <div class="c-feature__main">
+        <?php
+            pattern('scopes/text', [
+                'content' => $item->excerpt()
+            ]);
+        ?>
+    </div>
+    <?php endif; ?>
 </article>
--- src/patterns/common/result/result.html.php
+++ src/patterns/common/result/result.html.php
@@ -1,19 +1,20 @@
 <article class="c-result">
-    <a class="c-result__body" href="<?= $item->url() ?>">
-        <header class="c-result__header">
-            <?= brick('h'.$level)->html($item->title())->addClass('c-result__title') ?>
-        </header>
+    <header class="c-result__header">
+        <?php
+            $title = html::a($item->url(), $item->title());
+            echo brick('h'.$level)->html($title)->addClass('c-result__title')
+        ?>
+    </header>
 
-        <div class="c-result__main">
-            <?php
-                pattern('scopes/text', [
-                    'content' => !$item->desc()->empty() ? $item->desc() : excerpt($item->text(), $length=240)
-                ]);
-            ?>
-        </div>
+    <div class="c-result__main">
+        <?php
+            pattern('scopes/text', [
+                'content' => !$item->desc()->empty() ? $item->desc() : excerpt($item->text(), $length=240)
+            ]);
+        ?>
+    </div>
 
-        <footer class="c-result__footer">
-            <p><?= str::ucfirst($item->intendedTemplate()) ?></p>
-        </footer>
-    </a>
+    <footer class="c-result__footer">
+        <p><?= $item->uri() ?></p>
+    </footer>
 </article>
