234487ad16ab9f9d77a4b44fa21c0e285b63425e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-12-29 10:44:00 +0000

    Remove no longer used modules

--- src/patterns/shared/scripts/app.js
+++ src/patterns/shared/scripts/app.js
@@ -1,13 +1,9 @@
 // Load dependancies
-// DISABLED: import turbolinks from 'turbolinks';
 import loadWebfonts from './modules/webfont-loader';
 import geo from './modules/geo';
 import search from './modules/search';
-// REVIEW: import typography from './modules/typography';
 
 // Run
-// DISABLED: turbolinks.start();
 loadWebfonts();
 geo();
 search();
-// REVIEW: typography();
