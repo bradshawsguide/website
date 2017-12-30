bc39490b34a2a9fd1abfdfe70eda0606c19cac89
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-12-30 20:31:13 +0000

    Correct heading level for title on homepage

--- src/patterns/global/banner/banner.html.php
+++ src/patterns/global/banner/banner.html.php
@@ -1,18 +1,18 @@
 <header class="c-banner<?php e($page->isHomePage(), ' c-banner--home') ?>">
+<?php if ($page->isHomePage()): ?>
+    <h1 class="c-banner__masthead">
+        <span>Bradshaw’s Guide</span>
+        <span>for tourists in</span>
+        <span>Great Britain and&#160;Ireland</span>
+    </h1>
+<?php else: ?>
     <p class="c-banner__title">
-    <?php if ($page->isHomePage()): ?>
-        <span class="c-banner__masthead">
-            <span>Bradshaw’s Guide</span>
-            <span>for tourists in</span>
-            <span>Great Britain and&#160;Ireland</span>
-        </span>
-    <?php else: ?>
         <a class="c-banner__homelink" href="<?= url() ?>" rel="home">
             Bradshaw’s <span>Guide</span>
         </a>
-    <?php endif ?>
     </p>
-    <?php if ($page->isHomePage()): ?>
-        <p class="c-banner__edition">1866 Edition</p>
-    <?php endif ?>
+<?php endif ?>
+<?php if ($page->isHomePage()): ?>
+    <p class="c-banner__edition">1866 Edition</p>
+<?php endif ?>
 </header>
