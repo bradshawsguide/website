10098f0ed70230381335a4b4f100db63f31cbac7
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-18 22:34:15 +0000

    Use sitemap component instead of plugin

--- src/config/config.php
+++ src/config/config.php
@@ -33,6 +33,15 @@ return [
                 $txt = snippet('app/robots', [], true);
                 return new Response($txt, 'txt');
             }
+        ],
+        [
+            'pattern' => 'sitemap.xml',
+            'action'  => function() {
+                $pages = site()->index()->listed();
+                $ignore = kirby()->option('sitemap.ignore', ['error']);
+                $xml = snippet('app/sitemap', compact('pages', 'ignore'), true);
+                return new Response($xml, 'application/xml');
+            }
         ]
     ]
 ];
