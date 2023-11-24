ebdbdcf2c7c22f16e173f5d3653d4e0dc3586bcd
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-24 19:00:17 +0000

    Reorganise custom element scripts

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,7 +1,7 @@
 import '../styles/app.css';
 
-import { DialogElement } from './modules/dialog-element';
-import { DialogToggleElement } from './modules/dialog-toggle-element';
+import { DialogElement } from './custom-elements/dialog.js';
+import { DialogToggleElement } from './custom-elements/dialog-toggle.js';
 import geo from './modules/geo';
 
 customElements.define('b-dialog', DialogElement);
