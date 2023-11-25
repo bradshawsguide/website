0b77122d0abf43e1b9b5621793f2b7e71971f0fb
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-25 17:26:27 +0000

    Delete icon component

--- site/snippets/icon.php
+++
@@ -1,4 +0,0 @@
-<svg class="c-icon" height="<?= isset($size) ? $size : 16 ?>" width="<?= isset($size) ? $size : 16 ?>" role="img" <?= isset($label) ? 'aria-label="'.$label.'"' : 'aria-hidden="true"' ?>>
-    <use xlink:href="/assets/vectors/icons.svg#<?= $glyph?>"></use>
-</svg>
-
