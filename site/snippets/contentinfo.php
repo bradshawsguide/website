ca30a3f84140a7f22df3f45e0153f2acbc31a59a
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-25 01:38:03 +0000

    Refactor contentinfo component

--- site/snippets/contentinfo.php
+++ site/snippets/contentinfo.php
@@ -1,32 +1,18 @@
 <footer class="c-contentinfo">
-    <div class="c-contentinfo__social">
-        <a rel="me" href="https://micro.blog/bradshawsguide">
-            <?php snippet('icon', [
-                'glyph' => 'microblog',
-                'label' => 'Follow George Bradshaw on Micro.blog',
-                'size' => 24
-            ]) ?>
-        </a>
-        <a rel="me" href="https://github.com/bradshawsguide">
-            <?php snippet('icon', [
-                'glyph' => 'github',
-                'label' => 'Contribute on GitHub',
-                'size' => 24
-            ]) ?>
-        </a>
-        <a rel="me" href="https://foursquare.com/bradshawsguide">
-            <?php snippet('icon', [
-                'glyph' => 'foursquare',
-                'label' => 'Follow George Bradshaw on Foursquare',
-                'size' => 24
-            ]) ?>
-        </a>
-    </div>
-
-    <p class="c-contentinfo__rights">
-        <small>
-            ©&#160;<time datetime="2018" title="2018">MMXVIII</time> <a rel="author" href="<?= $site->publisher_url() ?>"><?= $site->publisher() ?></a>.
-            Content:&#160;<a rel="license" href="<?= $site->license_url() ?>"><?= $site->license() ?></a>
-        </small>
-    </p>
+    <small>
+        ©&#160;<time datetime="2018" title="2018">MMXVIII</time> <a rel="author" href="<?= $site->publisher_url() ?>"><?= $site->publisher() ?></a>.
+        Content:&#160;<a rel="license" href="<?= $site->license_url() ?>"><?= $site->license() ?></a>
+    </small>
+    <a rel="me" href="https://micro.blog/bradshawsguide">
+        <b-icon name="microblog"></b-icon>
+        <b-visually-hidden>Follow George Bradshaw on Micro.blog</b-visually-hidden>
+    </a>
+    <a rel="me" href="https://github.com/bradshawsguide">
+        <b-icon name="github"></b-icon>
+        <b-visually-hidden>Contribute on GitHub</b-visually-hidden>
+    </a>
+    <a rel="me" href="https://foursquare.com/bradshawsguide">
+        <b-icon name="foursquare"></b-icon>
+        <b-visually-hidden>Follow George Bradshaw on Foursquare</b-visually-hidden>
+    </a>
 </footer>
