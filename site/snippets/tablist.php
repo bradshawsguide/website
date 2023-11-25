ef12ccd1a1739b6541f4a34e95fc9adf2e63347d
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-25 17:50:53 +0000

    Refactor tablist component

--- site/snippets/tablist.php
+++ site/snippets/tablist.php
@@ -1,15 +1,19 @@
 <nav class="c-tablist" aria-label="<?= $title ?>">
-    <ul class="c-tablist__items">
-<?php foreach ($tabs as $tab): ?>
-        <li class="c-tablist__item">
-            <?php
-                $uri = $page->url().'/';
-                $param = $paramName.':'.$tab->uid();
-                $view = get('view') ? '?view='.get('view') : '';
-                $href = $uri.$param.$view;
-            ?>
-            <a href="<?= $href ?>" aria-label="<?= $tab->label() ?>"<?php e(param($paramName) == $tab->uid(), ' aria-current="page"') ?>><?= $tab->title() ?></a>
-        </li>
+    <ul>
+<?php foreach ($tabs as $tab):
+    $uri = $page->url().'/';
+    $param = $paramName.':'.$tab->uid();
+    $view = get('view') ? '?view='.get('view') : '';
+    $href = $uri.$param.$view;
+?>
+        <li><?=
+            Html::a($href, $tab->title(), [
+                'aria-current' => param($paramName) == $tab->uid()
+                    ? 'page'
+                    : false,
+                'aria-label' => $tab->label()
+            ])
+        ?></li>
 <?php endforeach ?>
     </ul>
 </nav>
