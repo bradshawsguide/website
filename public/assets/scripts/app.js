af2020912728c20b4ba2ec2c03389eb26bfa2026
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-25 01:24:59 +0000

    Add b-icon custom element

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -2,11 +2,13 @@ import '../styles/app.css';
 
 import { DialogElement } from './custom-elements/dialog.js';
 import { DialogToggleElement } from './custom-elements/dialog-toggle.js';
+import { IconElement } from './custom-elements/icon.js';
 import { VisuallyHiddenElement } from './custom-elements/visually-hidden.js';
 import geo from './modules/geo';
 
 customElements.define('b-dialog', DialogElement);
 customElements.define('b-dialog-toggle', DialogToggleElement);
+customElements.define('b-icon', IconElement);
 customElements.define('b-visually-hidden', VisuallyHiddenElement);
 
 geo();
---
+++ public/assets/scripts/custom-elements/icon.js
@@ -0,0 +1,40 @@
+import { css, LitElement, html } from 'lit';
+
+export class IconElement extends LitElement {
+  constructor () {
+    super();
+    this.name = '';
+    this.url = this.url || '/assets/vectors/icons.svg';
+  };
+
+  static properties = {
+    name: {
+      type: String,
+    },
+    url: {
+      type: String,
+      attribute: 'src'
+    },
+  };
+
+  static styles = css`
+    :host {
+      block-size: 1em;
+      box-sizing: content-box;
+      display: inline-block;
+      fill: currentcolor;
+      inline-size: 1em;
+    }
+
+    svg {
+      block-size: 100%;
+      display: block;
+      inline-size: 100%;
+    }`;
+
+  render() {
+    return html`<svg part="svg" width="1em" height="1em">
+      <use part="use" href="${this.url}#${this.name}"></use>
+    </svg>`;
+  };
+}
