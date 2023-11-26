c5e1276e7c95698cebee3ac986bd775215640253
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-26 18:06:21 +0000

    Autofocus search input in search dialog

--- site/snippets/search.php
+++ site/snippets/search.php
@@ -6,7 +6,7 @@
 
     <search>
         <form action="/search">
-            <input type="search" name="q" placeholder="e.g. Brighton, London Bridge…" value="<?= esc(get('q', '')) ?>" title="Search <?= $site->title() ?>">
+            <input type="search" name="q" autofocus placeholder="e.g. Brighton, London Bridge…" value="<?= esc(get('q', '')) ?>" title="Search <?= $site->title() ?>">
             <button type="submit">
                 <b-icon name="search"></b-icon>
                 <b-visually-hidden>Search</b-visually-hidden>
