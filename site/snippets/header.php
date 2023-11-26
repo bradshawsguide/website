a81cc945dd9110a86a84bd8df5c79e95f29e06b2
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-26 02:06:43 +0000

    Refactor header component

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,22 +1,12 @@
 <header class="<?= classList('c-header', $modifiers ?? null) ?>">
 <?php if (isset($parent)): ?>
-    <nav class="c-header__parent">
-        <?= $parent ?>
-    </nav>
+    <nav><?= $parent ?></nav>
 <?php endif ?>
-<?php if (isset($pretitle)): ?>
-    <p class="c-header__pretitle">
-        <?= $pretitle ?>
-    </p>
-<?php endif ?>
-    <?php
-        snippet('title', [
-            'title' => $title ?? $page->title(),
-            'level' => $level ?? 1,
-            'class' => 'c-header__title'
-        ]);
-    ?>
+    <h1><?=
+isset($pretitle) ? Html::tag('span', $pretitle) : '';
+e($title, kti($title), kti($page->title()));
+?></h1>
 <?php if (isset($subtitle)): ?>
-    <p class="c-header__subtitle"><?= $subtitle ?></p>
+    <p><?= $subtitle ?></p>
 <?php endif ?>
 </header>
