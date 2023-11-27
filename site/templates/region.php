cf7b3cfb856539d12563aef37da6acffafded947
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 22:15:57 +0000

    Change order of sections on region template

--- site/templates/region.php
+++ site/templates/region.php
@@ -16,15 +16,6 @@ if ($page->text()->isNotEmpty()) {
     ]);
 }
 
-if (size($page->featured())) {
-    snippet('section/list', [
-        'title' => 'Featured places',
-        'items' => $page->featured(),
-        'component' => 'feature',
-        'display' => 'grid'
-    ]);
-};
-
 if ($page->uid() != 'channel-islands') {
     $items = $page->children();
     if (size($items)) {
@@ -35,5 +26,15 @@ if ($page->uid() != 'channel-islands') {
         ]);
     }
 }
+
+if (size($page->featured())) {
+    snippet('section/list', [
+        'title' => 'Featured places',
+        'modifiers' => ['offset'],
+        'items' => $page->featured(),
+        'component' => 'feature',
+        'display' => 'grid'
+    ]);
+};
 
 snippet('foot');
