68fd077dba966b35c93168bca684abbcad2acbf2
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 23:48:51 +0000

    Refactor place component

--- site/snippets/place.php
+++ site/snippets/place.php
@@ -1,18 +1,15 @@
+<?php
+$title = Html::a($place->url(), $place->title());
+$suffixText = $suffix ? ' ('.$suffix.')' : '';
+?>
 <article class="c-place" id="<?= $place->uid() ?>">
+    <h3><?= $title.$suffixText ?></h3>
+
     <?php
-        $title = Html::a($place->url(), $place->title());
-        $suffixText = $suffix ? ' ('.$suffix.')' : '';
-
-        snippet('title', [
-            'title' => $title.$suffixText,
-            'level' => $level ?? 3,
-            'class' => 'c-place__title'
-        ]);
-
         if ($place->excerpt()) {
             snippet('scope/text', [
                 'content' => $place->excerpt()
             ]);
         }
-    ?>
+?>
 </article>
