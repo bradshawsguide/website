95da1063fa130ddf7ef95ade43286bd316781445
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 00:23:25 +0000

    Add locate custom element

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -2,12 +2,14 @@ import '../styles/app.css';
 
 import { DialogElement } from './custom-elements/dialog.js';
 import { IconElement } from './custom-elements/icon.js';
+import { LocateElement } from './custom-elements/locate.js';
 import { ToggleElement } from './custom-elements/toggle.js';
 import { VisuallyHiddenElement } from './custom-elements/visually-hidden.js';
 import geo from './modules/geo';
 
 customElements.define('b-dialog', DialogElement);
 customElements.define('b-icon', IconElement);
+customElements.define('b-locate', LocateElement);
 customElements.define('b-toggle', ToggleElement);
 customElements.define('b-visually-hidden', VisuallyHiddenElement);
 
---
+++ public/assets/scripts/custom-elements/locate.js
@@ -0,0 +1,61 @@
+import { css, LitElement, html } from 'lit';
+import { IconElement } from './icon.js';
+import { VisuallyHiddenElement } from './visually-hidden.js';
+
+export class LocateElement extends LitElement {
+  constructor () {
+    super();
+    this.label = 'Locate';
+    this.error = false;
+  };
+
+  static dependencies = {
+    'b-icon': IconElement,
+    'b-visually-hidden': VisuallyHiddenElement
+  };
+
+  static properties = {
+    label: {
+      type: String
+    },
+    error: {
+      type: Boolean,
+      reflect: true
+    }
+  };
+
+  static styles = css`
+    :host button {
+      background: none;
+      color: inherit;
+      border: 0;
+      display: flex;
+    }`;
+
+  #geoSuccess = (position) => {
+    const lat = position.coords.latitude.toFixed(4);
+    const lng = position.coords.longitude.toFixed(4);
+
+    window.location.href = '/search?g=' + lat + ',' + lng;
+  };
+
+  #geoError = () => {
+    this.error = true;
+  };
+
+  #locate() {
+    navigator.geolocation.getCurrentPosition(this.#geoSuccess, this.#geoError, {
+      maximumAge: 30_000,
+      timeout: 27_000
+    });
+  }
+
+  render() {
+    return html`
+      <button part="button" @click="${this.#locate}">
+        <b-icon part="icon" name="locate"></b-icon>
+        <b-visually-hidden part="label">${this.label}</b-visually-hidden>
+      </button>
+    `;
+  };
+}
