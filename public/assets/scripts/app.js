3349e6e369fc0836f3592027588e28e53e47b535
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 00:43:55 +0000

    Remove geo module

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -5,12 +5,9 @@ import { IconElement } from './custom-elements/icon.js';
 import { LocateElement } from './custom-elements/locate.js';
 import { ToggleElement } from './custom-elements/toggle.js';
 import { VisuallyHiddenElement } from './custom-elements/visually-hidden.js';
-import geo from './modules/geo';
 
 customElements.define('b-dialog', DialogElement);
 customElements.define('b-icon', IconElement);
 customElements.define('b-locate', LocateElement);
 customElements.define('b-toggle', ToggleElement);
 customElements.define('b-visually-hidden', VisuallyHiddenElement);
-
-geo();
