0f7e8a941468a3e15246bc4eade573d27c795139
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 21:14:18 +0000

    Refactor contentinfo component

--- site/snippets/contentinfo.php
+++ site/snippets/contentinfo.php
@@ -1,12 +1,8 @@
 <footer class="c-contentinfo">
     <small>
-        ©&#160;<time datetime="2018" title="2018">MMXVIII</time> <a rel="author" href="<?= $site->publisher_url() ?>"><?= $site->publisher() ?></a>.
-        Content:&#160;<a rel="license" href="<?= $site->license_url() ?>"><?= $site->license() ?></a>
+        ©&nbsp;<time datetime="2018" title="2018">MMXVIII</time> <a rel="author" href="<?= $site->publisher_url() ?>"><?= $site->publisher() ?></a>.
+        Content:&nbsp;<a rel="license" href="<?= $site->license_url() ?>"><?= $site->license() ?></a>
     </small>
-    <a rel="me" href="https://micro.blog/bradshawsguide">
-        <b-icon name="microblog"></b-icon>
-        <b-visually-hidden>Follow George Bradshaw on Micro.blog</b-visually-hidden>
-    </a>
     <a rel="me" href="https://github.com/bradshawsguide">
         <b-icon name="github"></b-icon>
         <b-visually-hidden>Contribute on GitHub</b-visually-hidden>
