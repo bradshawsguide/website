ed9417c307a5dce966d0c257864209f840459f07
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 21:57:18 +0000

    Refactor index component

--- site/snippets/index.php
+++ site/snippets/index.php
@@ -1,7 +1,7 @@
-<section class="c-index" id="<?= $letter ?>">
-    <h2 class="c-index__title"><?= Str::upper($letter) ?></h2>
+<article class="c-index" id="<?= $letter ?>">
+    <h2><?= Str::upper($letter) ?></h2>
     <?= snippet('list', [
         'items' => $items,
         'display' => $listDisplay ?? null
     ]); ?>
-</section>
+</article>
