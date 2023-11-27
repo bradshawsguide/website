3d1c99a57d81ba31976ead4ff738c3c5e9f12227
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-27 00:43:15 +0000

    Refactor inquire component to use b-locate

--- site/snippets/inquire.php
+++ site/snippets/inquire.php
@@ -1,18 +1,11 @@
-<form class="<?= classList('c-inquire', $modifiers ?? null) ?>" role="search" action="/search">
-    <fieldset class="c-inquire__body">
-        <legend class="c-inquire__header">
-            <span class="c-inquire__title"><?= $title ?></span>
-        </legend>
-        <div class="c-inquire__main">
-            <input class="c-inquire__input" type="search" name="q" placeholder="e.g. Brighton, Windsor Castle, Surrey…" value="<?= esc(get('q', '')) ?>">
-            <button class="c-inquire__button" type="submit">
-                <b-icon name="search"></b-icon>
-                <b-visually-hidden>Search</b-visually-hidden>
-            </button>
-            <button class="c-inquire__button" type="button" data-geo>
-                <b-icon name="locate"></b-icon>
-                <b-visually-hidden>Places near me</b-visually-hidden>
-            </button>
-        </div>
-    </fieldset>
-</form>
+<search class="<?= classList('c-inquire', $modifiers ?? null) ?>">
+    <form action="/search">
+        <label for="q"><?= $title ?></label>
+        <input class="c-inquire__input" type="search" id="q" name="q" placeholder="e.g. Brighton, Windsor Castle, Surrey…" value="<?= esc(get('q', '')) ?>">
+        <button type="submit">
+            <b-icon name="search"></b-icon>
+            <b-visually-hidden>Search</b-visually-hidden>
+        </button>
+        <b-locate label="Places near me"></b-locate>
+    </form>
+</search>
