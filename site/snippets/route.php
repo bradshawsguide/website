9e776cd1dd55a1734180c32a2ae152f0b9b48fe7
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-28 01:22:05 +0000

    Refactor route component

--- site/snippets/route.php
+++ site/snippets/route.php
@@ -1,15 +1,9 @@
 <article class="c-route" id="<?= $route->uid() ?>">
-    <?php
-        snippet('title', [
-            'title' => Html::a($route->url(),
-                [$route->lineTitle().'<b-icon name="next"></a>']
-            ),
-            'level' => $level ?? 3,
-            'class' => 'c-route__title'
-        ]);
+    <h3><?= Html::a($route->url(), [
+        $route->lineTitle().'<b-icon name="next"></a>'
+    ]); ?></h3>
 
-        snippet('scope/text', [
-            'content' => $route->title()
-        ]);
-    ?>
+    <?= snippet('scope/text', [
+        'content' => $route->title()
+    ]); ?>
 </article>
