55d9885786e04d597c88d918b941d30c44d72fcc
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-29 01:05:20 +0000

    Refactor navigation scope component

--- site/snippets/scope/navigation.php
+++ site/snippets/scope/navigation.php
@@ -1,11 +1,12 @@
 <nav class="s-navigation" data-display="aside">
-<?php foreach ($items->listed() as $item): ?>
-    <?php $title = $item->title_short()->isNotEmpty()
+<?php foreach ($items->listed() as $item):
+    $title = $item->title_short()->isNotEmpty()
         ? $item->title_short()
-        : $item->title(); ?>
-    <a href="<?= $item->url() ?>"<?php e(
-    $item->isActive(),
-    ' aria-current="page"'
-); ?>><?= kti($title) ?></a>
-<?php endforeach; ?>
+        : $item->title();
+
+    echo Html::tag('a', [kti($title)], [
+        "aria-current" => $item->isActive() ? "page" : false,
+        "href" => $item->url(),
+    ], "    ", 1)."\n";
+endforeach; ?>
 </nav>
