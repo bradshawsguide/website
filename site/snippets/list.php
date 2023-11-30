2baf1e9c1cdd32730ced9162aa75e062f12aade8
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-11-30 21:50:01 +0000

    Refactor list component

--- site/snippets/list.php
+++ site/snippets/list.php
@@ -1,7 +1,11 @@
-<ul class="c-list" data-display="<?= $display ?>">
-<?php foreach ($items as $item): ?>
-    <li><?= isset($component)
-        ? snippet($component, ["item" => $item])
-        : Html::a($item->url(), [kti($item->title())]) ?></li>
-<?php endforeach; ?>
+<ul class="c-list"<?php e($display, ' data-display="' . $display . '"'); ?>>
+    <?php foreach ($items as $item): ?>
+        <li>
+            <?php if (isset($component)): ?>
+                <?= snippet($component, ["item" => $item]) ?>
+            <?php else: ?>
+                <a href="<?= $item->url() ?>"><?= kti($item->title()) ?></a>
+            <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
 </ul>
