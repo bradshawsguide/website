4d90d7b65d7755ad6529720fed9a8e972cc54c0a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-11-30 22:45:05 +0000

    Refactor traverse component

--- site/snippets/traverse.php
+++ site/snippets/traverse.php
@@ -1,27 +1,29 @@
 <nav class="c-traverse" aria-labelledby="traverse-title">
     <h2 id="traverse-title">
-        <?= Html::a($page->parent()->url(), $page->parent()->title()) ?>
+        <a href="<?= $page->parent()->url() ?>">
+            <?= $page->parent()->title() ?>
+        </a>
     </h2>
     <ul>
-<?php if ($page->hasPrev()): ?>
-        <li>
-            <a rel="prev" href="<?= $page->prev()->url() ?>">
-                <b-icon name="prev"></b-icon>
-                <b-visually-hidden><?= kti(
-                    $page->prev()->shortTitle()
-                ) ?></b-visually-hidden>
-            </a>
-        </li>
-<?php endif; ?>
-<?php if ($page->hasNext()): ?>
-        <li>
-            <a rel="next" href="<?= $page->next()->url() ?>">
-                <b-icon name="next"></b-icon>
-                <b-visually-hidden><?= kti(
-                    $page->next()->shortTitle()
-                ) ?></b-visually-hidden>
-            </a>
-        </li>
-<?php endif; ?>
+        <?php if ($page->hasPrev()): ?>
+            <li>
+                <a href="<?= $page->prev()->url() ?>" rel="prev">
+                    <b-icon name="prev"></b-icon>
+                    <b-visually-hidden><?= kti(
+                        $page->prev()->shortTitle()
+                    ) ?></b-visually-hidden>
+                </a>
+            </li>
+        <?php endif; ?>
+        <?php if ($page->hasNext()): ?>
+            <li>
+                <a href="<?= $page->next()->url() ?>" rel="next">
+                    <b-visually-hidden><?= kti(
+                        $page->next()->shortTitle()
+                    ) ?></b-visually-hidden>
+                    <b-icon name="next"></b-icon>
+                </a>
+            </li>
+        <?php endif; ?>
     </ul>
 </nav>
