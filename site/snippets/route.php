eb02f5071cfd07faa9b56c660178a0391b4018d4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-11-30 22:15:42 +0000

    Refactor route component

--- site/snippets/route.php
+++ site/snippets/route.php
@@ -1,7 +1,10 @@
 <article class="c-route" id="<?= $route->uid() ?>">
-    <h3><?= Html::a($route->url(), [
-        $route->lineTitle() . '<b-icon name="next"></a>',
-    ]) ?></h3>
+    <h3>
+        <a href="<?= $route->url() ?>">
+            <?= $route->lineTitle() ?>
+            <b-icon name="next">
+        </a>
+    </h3>
 
     <?= snippet("scope/text", [
         "content" => $route->title(),
