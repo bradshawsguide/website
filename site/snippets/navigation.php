a39aa02ed19cbd383dae8618a9bec0389da2ffc6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-01 00:15:50 +0000

    Refactor navigation component

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,18 +1,16 @@
 <dialog open class="c-navigation" id="navigation" aria-labelledby="navigation-title">
     <b-toggle action="close" icon="close" label="Close menu"></b-toggle>
-
     <nav class="c-navigation__container">
         <h2 id="navigation-title">Explore <?= $site->title() ?></h2>
         <ul>
-<?php foreach ($site->children()->listed() as $item): ?>
-            <li><?= Html::a(
-                $item->url(),
-                [kti($item->title())],
-                [
-                    "aria-current" => $item->isOpen() ? "page" : false,
-                ]
-            ) ?></li>
-<?php endforeach; ?>
+            <?php foreach ($site->children()->listed() as $item): ?>
+                <li>
+                    <a href="<?= $item->url() ?>"
+                        <?= ariacurrent($item->isOpen()) ?>>
+                        <?= kti($item->title()) ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
 </dialog>
