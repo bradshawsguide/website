8968682ce5f9ae9bb57a88b14bd0616bcbb2266d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-01 00:14:45 +0000

    Refactor pagination component

--- site/snippets/pagination.php
+++ site/snippets/pagination.php
@@ -1,27 +1,27 @@
 <nav class="c-pagination" aria-label="Pagination">
     <ul class="c-pagination__items">
-<?php if ($pagination->hasPrevPage()): ?>
-        <li><?= Html::a(
-            $pagination->prevPageURL(),
-            ['<b-icon name="prev"/></b-icon>Previous'],
-            [
-                "aria-label" => "Previous results page",
-                "rel" => "prev",
-            ]
-        ) ?></li>
-<?php endif; ?>
-<?php foreach ($pagination->range(10) as $paging): ?>
-        <li><?= Html::a($pagination->pageURL($paging), $paging, [
-            "aria-current" => $paging == $pagination->page() ? "page" : false,
-            "aria-label" => "Go to page " . $paging,
-        ]) ?></li>
-<?php endforeach; ?>
-<?php if ($pagination->hasNextPage()): ?>
-        <li><?= Html::a(
-            $pagination->nextPageURL(),
-            ['<b-icon name="next"/></b-icon>Next'],
-            ["aria-label" => "Next results page", "rel" => "next"]
-        ) ?></li>
-<?php endif; ?>
+        <?php if ($pagination->hasPrevPage()): ?>
+            <li>
+                <a href="<?= $pagination->prevPageURL() ?>" rel="prev" aria-label="Previous results page">
+                    <b-icon name="prev"/></b-icon>Previous
+                </a>
+            </li>
+        <?php endif; ?>
+        <?php foreach ($pagination->range(10) as $paging): ?>
+            <li>
+                <a href="<?= $pagination->pageURL($paging) ?>"
+                    aria-label="<?= "Go to page {$paging}" ?>"
+                    <?= ariacurrent($paging == $pagination->page()) ?>>
+                    <?= $paging ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+        <?php if ($pagination->hasNextPage()): ?>
+            <li>
+                <a href="<?= $pagination->nextPageURL() ?>" rel="next" aria-label="Next results page">
+                    Next<b-icon name="next"/></b-icon>
+                </a>
+            </li>
+        <?php endif; ?>
     </ul>
 </nav>
