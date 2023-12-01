a45d8f1e85189e9566252c9ddf50de90d2eaaa99
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-01 00:15:41 +0000

    Refactor switch component

--- site/snippets/switch.php
+++ site/snippets/switch.php
@@ -1,13 +1,12 @@
 <nav class="c-switch" aria-label="<?= $title ?>">
     <ul>
-<?php foreach ($switches as $switch):
-    $href = "?" . $queryName . "=" . $switch["uid"]; ?>
-        <li><?= Html::tag("a", $switch["label"], [
-            "href" => $href,
-            "aria-current" =>
-                get($queryName) == $switch["uid"] ? "page" : false,
-        ]) ?></li>
-<?php
-endforeach; ?>
+        <?php foreach ($switches as $switch): ?>
+            <li>
+                <a href="<?= "?{$queryName}={$switch["uid"]}" ?>"
+                    <?= ariacurrent(get($queryName) == $switch["uid"]) ?>>
+                    <?= $switch["label"] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </nav>
