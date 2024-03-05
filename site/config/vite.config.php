73f3903fb66c872379c1dbd30455b9b8b9b0c7a8
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-03-05 11:57:14 +0000

    Update Vite config

--- site/config/vite.config.php
+++ site/config/vite.config.php
@@ -2,6 +2,7 @@
 // This is an auto-generated file. Please avoid making changes here.
 // Configure your settings in the "vite.config.js" file instead.
 return [
+  'rootDir' => 'public',
   'outDir' => 'public/dist',
   'assetsDir' => 'assets',
   'legacy' => false
